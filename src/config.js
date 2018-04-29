2101bbf3d6f598de9c9ba8a9ab7ca07eeecbd7b0
Kevin Bieri <bierike@gmail.com>
2018-04-29 19:51:09 +0200

    Use custom map style

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const mapStyle = 'mapbox://styles/mapbox/light-v9';
+export const mapStyle = 'mapbox://styles/bierik/cjgkyw2qn007a2spkba7t9994';
 export const mapCenter = [0, 45];
 export const mapZoom = 1.5;
 export const mapAccessToken = 'pk.eyJ1IjoiYmllcmlrIiwiYSI6ImNqZno0MWl0bjN0aDIzNHBkdmJteXJnbzIifQ.Zu6EhGsMcYvdXbjO3LJaHA';
