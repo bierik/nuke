377f85a6579d26d0e4275a29d3391b0353129193
robin.christen <robin.christen@students.fhnw.ch>
2018-05-31 16:44:28 +0200

    fixing historgram data

--- src/api/store.js
+++ src/api/store.js
@@ -11,14 +11,23 @@ export async function Store() {
   function getNukesPerYear() {
     const defaultCountryValues = Object.keys(colorMap)
       .reduce((acc, c) => Object.assign(acc, { [c]: 0 }), {});
+
     const nukesPerYear = groupBy(
       nukeData,
       d => new Date(Number.parseInt(d.time, 10)).getFullYear(),
     );
+
+    for (let i = 1945; i <= 2010; i += 1) {
+      if (!(nukesPerYear[i])) {
+        nukesPerYear[i] = [];
+      }
+    }
+
     const nukesPerYearPerCountry = mapValues(
       nukesPerYear,
       d => mapValues(groupBy(d, 'country'), e => e.length),
     );
+
     const nukesPerYearPerCountrySum = Object.keys(nukesPerYearPerCountry).map((d) => {
       const row = nukesPerYearPerCountry[d];
       const total = Object.keys(row).reduce((acc, r) => row[r] + acc, 0);
